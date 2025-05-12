2e86552d4b3ecbcd69ec3c8fa28e6485cb322183
Solome Zewdu <solomezewdu125@gmail.com>
2025-05-12 23:30:06 +0300

    deleted unused import on BrowseResearchScreen.kt

--- app/src/main/java/com/project/collabrix/ui/screens/Student/Pages/BrowseResearchScreen.kt
+++ app/src/main/java/com/project/collabrix/ui/screens/Student/Pages/BrowseResearchScreen.kt
@@ -3,13 +3,11 @@ package com.project.collabrix.ui.screens.Student.Pages
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
